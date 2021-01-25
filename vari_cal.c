0beab9401f1d53b7252452150d66764e31aa30e3
MoonOld <431002206@qq.com>
2021-01-25 14:04:11 +0800

    计算部分

---
+++ vari_cal.c
@@ -0,0 +1,21 @@
+#include stdio.h
+#include math.h
+#define exp1
+
+#ifdef exp1
+int main()
+{
+    float a,b;
+    scanf("%f %f",&a,&b);
+    printf("%f\n",a+b);
+    return 0;
+}
+#endif
+
+#ifdef area
+int main(){
+    float r,h;
+    scanf("%f %f",&r,&h);
+    printf("Area = %.3f\n",2*PI*r*r+2*PI*r*h);
+}
+#endif
