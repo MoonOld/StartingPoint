04de5524a54cc33176859dbbe680b86449d00e02
MoonOld <431002206@qq.com>
2021-01-25 13:57:12 +0800

    计算部分

--- calculation.c
+++ calculation.c
@@ -1,20 +1,29 @@
 //exp1~4
 #define exp1
 #include stdio.h
+#include math.h
 int main(){
     printf("%f",
 #ifdef exp1
 3-4
-#endif  
+#endif
+
 #ifdef exp2
 5*6
-#endif    
+#endif
+
 #ifdef exp3
 8/4.0
-#endif    
+#endif
+
 #ifdef exp4
 8/5.0
+#endif
+
+#ifdef exp5
+1+2*sqrt(3)/(5-0.1)
 #endif      
+    
     )
     return 0;
 }
